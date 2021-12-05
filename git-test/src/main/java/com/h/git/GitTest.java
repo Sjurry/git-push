12e27f7e1b722f378fb80e4b27e180f19b5408c8
Jurry <2915345354@qq.com>
2021-12-05 15:31:53 +0800

    master commit

--- git-test/src/main/java/com/h/git/GitTest.java
+++ git-test/src/main/java/com/h/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("savc");
         System.out.println("hello3");
+        System.out.println("master");
     }
 }
